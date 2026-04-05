cc5b812ef1349eb98b15c57e853cba2109bd14be
Imtiyaz Haider <imtiyazhaider11@gmail.com>
2026-04-05 18:13:28 +0530

    fix port binding

--- app.py
+++ app.py
@@ -56,5 +56,8 @@ def home():
     return render_template("index.html", message=message)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
